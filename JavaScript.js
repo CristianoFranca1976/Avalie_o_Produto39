b7130d7427271c062084850b3ff9ed60ff556972
Cristiano Ferro De Franca <135919856+CristianoFranca1976@users.noreply.github.com>
2023-09-28 19:04:37 +0100

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,36 @@
+var StarVazio = "assets/StarVazio.svg";
+var StarCheio = "assets/StarCheio.svg";
+function Trocar1() {
+  document.getElementById("img1").src = StarCheio;
+  let Aux = StarVazio;
+  StarVazio = StarCheio;
+  StarCheio = Aux;
+}
+
+function Trocar2() {
+  document.getElementById("img2").src = StarCheio;
+  let Aux = StarVazio;
+  StarVazio = StarCheio;
+  StarCheio = Aux;
+}
+
+function Trocar3() {
+  document.getElementById("img3").src = StarCheio;
+  let Aux = StarVazio;
+  StarVazio = StarCheio;
+  StarCheio = Aux;
+}
+
+function Trocar4() {
+  document.getElementById("img4").src = StarCheio;
+  let Aux = StarVazio;
+  StarVazio = StarCheio;
+  StarCheio = Aux;
+}
+
+function Trocar5() {
+  document.getElementById("img5").src = StarCheio;
+  let Aux = StarVazio;
+  StarVazio = StarCheio;
+  StarCheio = Aux;
+}
---
+++ JavaScript1.js
@@ -0,0 +1,3 @@
+function resetPagina() {
+  location.reload();
+}
